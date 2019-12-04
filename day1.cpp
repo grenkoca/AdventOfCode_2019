ac4fbcb2203de08dec8a8c4c08294db3b853ec35
grenkoca <grenkoca@gmail.com>
2019-12-04 20:26:39 +0100

    Day 1 added

---
+++ day1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+int main() {
+    int total=0;
+    string str;   
+    ifstream in("input");
+    double num;
+
+    while(std::getline(in, str)){
+        num = std::stod(str);
+
+        num = num/3;    
+        num = floor(num);
+        num = num - 2;
+        total += num;        
+    } 
+    cout << total;
+    return total;   
+}
